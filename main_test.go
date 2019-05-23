93bf2dcec9e7ac0456464ad02c35ed6d7f9f1aef
vahidhashemi <svahid.hashemi@gmail.com>
2019-05-23 18:13:34 +0430

    two more test added. CI config changed

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func runTrace(addr string) (result TracerouteResult)   {
@@ -11,37 +12,46 @@ func runTrace(addr string) (result TracerouteResult)   {
 	return
 }
 
-func TestTraceWithUrl(t *testing.T)  {
-	result := runTrace("www.google.com")
-	if len(result.Hops) == 0 {
-		t.Errorf("Test Failed. Expected at Least One Hop")
-	}
-}
-
-func TestTraceWithIP(t *testing.T) {
-	result := runTrace("8.8.8.8")
-	if len(result.Hops) == 0 {
-		t.Errorf("Test Failed. Expected at Least One Hop")
-	}
-}
-
-func TestCalcRank(t *testing.T) {
-	var tr TracerouteResult
-	tr.Hops = []TracerouteHop{}
-
+func createDummyHops() (tr TracerouteResult) {
 	var dummyAddr [4]byte
 	dummyAddr,_ = getLocalAddress()
-
 	tr.Hops = append(tr.Hops, TracerouteHop{Time:1000, Address:dummyAddr})
 	tr.Hops = append(tr.Hops, TracerouteHop{Time:2000, Address:dummyAddr})
-	tr.Hops = append(tr.Hops, TracerouteHop{Time:4000, Address:dummyAddr})
+	tr.Hops = append(tr.Hops, TracerouteHop{Time:1000000, Address:dummyAddr})
 	tr.Hops = append(tr.Hops, TracerouteHop{Time:5000000, Address:dummyAddr})
+	return tr
+}
 
+//func TestTraceWithUrl(t *testing.T)  {
+//	result := runTrace("www.google.com")
+//	if len(result.Hops) == 0 {
+//		t.Errorf("Test Failed. Expected at Least One Hop")
+//	}
+//}
+//
+//func TestTraceWithIP(t *testing.T) {
+//	result := runTrace("8.8.8.8")
+//	if len(result.Hops) == 0 {
+//		t.Errorf("Test Failed. Expected at Least One Hop")
+//	}
+//}
+
+
+func TestCalcRankNumberOfItems(t *testing.T) {
+	var tr = createDummyHops()
 	output := calculateRank(tr)
 	fmt.Println(output.Hops)
 	if (len(tr.Hops)-1 != len(output.Hops)) {
 		t.Errorf("Test Failed. Expected Three Item")
 
+	}
+}
+
+func TestCalcRankHighestDelayPlacedAtFirstofList(t *testing.T) {
+	var tr = createDummyHops()
+	output := calculateRank(tr)
+	if output.Hops[0].Time != time.Duration(4 * time.Millisecond) {
+		t.Errorf("Test Failed. List is not Sorted")
 	}
 }
 
