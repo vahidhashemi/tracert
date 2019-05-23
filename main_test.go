0842e319f90c898834e41f50851e98a8b75589a3
vahidhashemi <svahid.hashemi@gmail.com>
2019-05-23 14:53:37 +0430

    added TestCalcRank

--- main_test.go
+++ main_test.go
@@ -1,9 +1,12 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func runTrace(addr string) (result TracerouteResult)   {
-	options := TracerouteOptions{ }
+	options := TracerouteOptions{ TimeoutMs:100,Retries:1}
 	result = trace(addr, &options)
 	return
 }
@@ -21,3 +24,25 @@ func TestTraceWithIP(t *testing.T) {
 		t.Errorf("Test Failed. Expected at Least One Hop")
 	}
 }
+
+func TestCalcRank(t *testing.T) {
+	var tr TracerouteResult
+	tr.Hops = []TracerouteHop{}
+
+	var dummyAddr [4]byte
+	dummyAddr,_ = getLocalAddress()
+
+	tr.Hops = append(tr.Hops, TracerouteHop{Time:1000, Address:dummyAddr})
+	tr.Hops = append(tr.Hops, TracerouteHop{Time:2000, Address:dummyAddr})
+	tr.Hops = append(tr.Hops, TracerouteHop{Time:4000, Address:dummyAddr})
+	tr.Hops = append(tr.Hops, TracerouteHop{Time:5000000, Address:dummyAddr})
+
+	output := calculateRank(tr)
+	fmt.Println(output.Hops)
+	if (len(tr.Hops)-1 != len(output.Hops)) {
+		t.Errorf("Test Failed. Expected Three Item")
+
+	}
+}
+
+
