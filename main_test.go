dd9d58d76ba86e6480943bacf6a99ff178631388
vahidhashemi <svahid.hashemi@gmail.com>
2019-05-23 12:28:35 +0430

    test added

---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func runTrace(addr string) (result TracerouteResult)   {
+	options := TracerouteOptions{ }
+	result = trace("8.8.8.8", &options)
+	return
+}
+
+func TestTraceWithUrl(t *testing.T)  {
+	result := runTrace("www.google.com")
+	if len(result.Hops) == 0 {
+		t.Errorf("Test Failed. Expected at Least One Hop")
+	}
+}
+
+func TestTraceWithIP(t *testing.T) {
+	result := runTrace("8.8.8.8")
+	if len(result.Hops) == 0 {
+		t.Errorf("Test Failed. Expected at Least One Hop")
+	}
+}
